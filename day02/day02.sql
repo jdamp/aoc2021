a4a498076fde55c47951679434aaff8ea33bbb3d
Johannes Damp <jodamp@uni-mainz.de>
2021-12-21 16:02:06 +0100

    Add solution of part 2

--- day02/day02.sql
+++ day02/day02.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS instructions;
 
 CREATE TABLE instructions (
+       id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
        direction  varchar(8),
        amount integer NOT NULL
 );
@@ -12,13 +13,15 @@ CREATE TABLE instructions (
 -- up      -> x=x,    y=y-dy
 -- down    -> x=x,    y=y+dy
 
+\echo 'Part 1 results:';
 WITH velocities_x AS (
      SELECT amount, direction,
           CASE WHEN direction = 'forward' THEN amount
 	       ELSE 0 END
 	  AS vx
      FROM instructions
-), velocities AS (
+),
+velocities AS (
      SELECT amount, direction, vx,
           CASE WHEN direction = 'down' THEN amount
 	       WHEN direction = 'up' THEN -1*amount
@@ -27,3 +30,32 @@ WITH velocities_x AS (
      FROM velocities_x
 )
 SELECT SUM(vx)*SUM(vy) from velocities;
+
+
+-- Part 02
+WITH instructions_with_aim AS (
+     SELECT id, amount, direction,
+     	    SUM(
+	         CASE WHEN direction = 'down' THEN amount
+	    	 WHEN direction = 'up'   THEN -1*amount
+		 ELSE 0 END
+	    	 ) OVER (order by id)
+	    AS aim
+     FROM instructions
+),
+positions_with_aim AS (
+     SELECT id, amount, direction, aim,
+     	    SUM(
+	         CASE WHEN direction = 'forward' THEN amount
+		 ELSE 0 END
+	    	 ) OVER (order by id)
+	    AS horizontal_position,
+
+	    SUM(
+	         CASE WHEN direction = 'forward' THEN aim*amount
+		 ELSE 0 END
+	    	 ) OVER (order by id)
+	    AS depth
+     FROM instructions_with_aim
+)
+SELECT horizontal_position * depth FROM positions_with_aim ORDER BY ID DESC LIMIT 1;
