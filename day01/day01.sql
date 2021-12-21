eb2dacae6c965bb2c8858948905764ba6f1850af
Johannes Damp <jodamp@uni-mainz.de>
2021-12-21 15:00:24 +0100

    Add solution of day01 part2

--- day01/day01.sql
+++ day01/day01.sql
@@ -13,4 +13,14 @@ FROM day01.depths d1
 INNER JOIN day01.depths d2
       ON (d1.id = d2.id-1)
 WHERE d2.depth-d1.depth > 0;
-      
+
+-- A three-measurement sliding window as defined as:
+-- w(i) = d(i) + d(i+1) + d(i+2), where d(i) is the depth in column i.
+-- Therefore, the difference of neighbouring windows can be simply expressed
+-- as w(i+1)-w(i) = [d(i+1) + d(i+2) + d(i+3)] - [d(i) + d(i+1) + d(i+2)] = d(i+3) - d(i)
+
+WITH lagDepths AS(
+    SELECT depth as di3, LAG(depth, 3) OVER (ORDER BY id ASC) as di
+    FROM day01.depths
+)
+SELECT COUNT(*) FROM lagDepths WHERE di3-di > 0;
