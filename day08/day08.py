4dfb3031509bd3ee9e2339920ea6886d5c771005
Johannes Damp <jodamp@uni-mainz.de>
2022-01-02 13:49:14 +0100

    Add solution code for day08

--- day08/day08.py
+++ day08/day08.py
@@ -1,22 +1,19 @@
 import numpy as np
-from numpy.core.defchararray import count
-from numpy.core.fromnumeric import shape
-from numpy.lib.arraysetops import unique
-from numpy.linalg import inv, pinv
 from typing import Generator
+from collections import Counter
 
 def parse_input(filename: str) -> Generator[tuple[list[str], list[str]], None, None]:
     with open(filename, 'r') as f:
         for line in f.readlines():
             # get rid of spaces around the splitting character
             line = line.replace(" | ", "|")
-            patterns, output = line.strip().split("|")
-            yield patterns.split(" "), output.split(" ")
+            pattern, output = line.strip().split("|")
+            patterns = ["".join(sorted(p)) for p in pattern.split(" ")]
+            outputs = ["".join(sorted(o)) for o in output.split(" ")]
+            yield patterns, outputs
 
-def index_to_letter(i: int) -> str:
-    return chr(ord('a')+i)
-
-def code_to_matrix(codes: list[str]) -> np.array:
+def pattern_to_matrix(codes: list[str]) -> np.array:
+    # transforms pattern to 10x7 matrix indicating which letter occurs in which digit
     out = np.zeros(shape=(10, 7))
     for row, code in enumerate(codes):
         for i in range(ord('a'), ord('g')+1):
@@ -24,6 +21,15 @@ def code_to_matrix(codes: list[str]) -> np.array:
                 out[row, i-ord('a')] = 1
     return out
 
+
+def count_matrix(M: np.ndarray) -> np.ndarray:
+    """
+    Return permutation of rows according to counts of letters in all 10 digits
+    """
+    letter_counts = M.sum(axis=0)
+    A = np.tile(np.arange(0, 7), 10).reshape(10, 7)
+    M_count =  letter_counts[A]*M
+    return M_count.sum(axis=1).argsort()
 
 
 # Original Mapping (a,b,c,d,e,f,g)->(0,1,2,3,4,5,6,7,8,9)
@@ -40,22 +46,16 @@ M = np.array([
     [1,1,1,1,0,1,1], # 9
 ])
 
-def code_matrix(M):
-    letter_counts = M.sum(axis=0)
-    A = np.tile(np.arange(0, 7), 10).reshape(10, 7)
-    return letter_counts[A]*M
+if __name__ == "__main__":
+    numbers = count_matrix(M)
+    counter = Counter() # part a
+    digit_numbers = [] # part b
+    for pattern, output in parse_input("input.txt"):
+        incount = count_matrix(pattern_to_matrix(pattern))
+        pattern_map = dict(zip(np.array(pattern)[incount], numbers))
+        decoded = [pattern_map[o] for o in output]
+        digit_numbers.append(int("".join(map(str, decoded))))
+        counter.update(decoded)
 
-def P7(M):
-    col_idx = M.sum(axis=0).argsort()
-    return np.eye(7)[col_idx]
-
-def P10(M):
-    col_idx = M.sum(axis=1).argsort()
-    return np.eye(10)[col_idx]    
-
-code = code_matrix(M)
-print(P10(code)@code@P7(code).T)
-
-for pattern, output in parse_input("testinput.txt"):
-    incode = code_matrix(code_to_matrix(pattern))
-    print(P10(incode)@incode@P7(incode).T)
+print(f"Part 1: {counter[1]+counter[4]+counter[7]+counter[8]}")
+print(f"Part 2: {sum(n)}")
